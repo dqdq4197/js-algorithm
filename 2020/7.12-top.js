ed4f8722cb8ecbdb1c845ab1c0dad6b21da7947f
choiheesu <dqdq4197@naver.com>
2020-07-12 23:48:08 +0900

    프로그래머스 level 2

---
+++ 2020/7.12-top.js
@@ -0,0 +1,23 @@
+function solution(heights) {
+    var answer = [];
+    heights = heights.reverse();
+   for(let i=0; i <= heights.length; i++) {
+       let check = 0;
+       
+       for(let v = i+1; v <= heights.length; v++) {
+           if(check === 1) {
+                break
+            } else {
+                if(heights[i] <heights[v]) {
+                   answer.push(heights.length-v);
+                    console.log(heights[i])
+                   check=1;
+                } else if(v === heights.length) {
+                    answer.push(0);
+                    check=1;
+                }
+            }
+       }
+   }
+    return answer.reverse();
+}
