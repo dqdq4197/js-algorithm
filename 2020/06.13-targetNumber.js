c8f9c56518e852871cabc6508907b0bced4ea395
choiheesu <dqdq4197@naver.com>
2020-07-29 21:45:18 +0900

    targetNum

--- 2020/06.13-targetNumber.js
+++ 2020/06.13-targetNumber.js
@@ -18,6 +18,26 @@ function solution(numbers, target) {
     
     return answer;
 }
+// 두번 째 답 
+// function solution(numbers, target) {
+//     var answer = 0;
+//     const recursion = (arr,index) => {
+//         let sum = arr.reduce((a,b) => a+b);
+//         if(sum === target) {
+//             answer ++;
+//         }
+//         for(let i = index; i < arr.length; i++) {
+//             arr[i] *= -1;
+//             recursion(arr, i + 1);
+//             arr[i] *= -1;
+//         }
+//         if(index === arr.length ) return ;
+//         index ++;
+//     }
+//     recursion(numbers, 0);
+                    
+//     return answer;
+// }
 
 
 [['ICN','BOO' ], [ 'ICN', 'COO' ], [ 'COO', 'DOO' ], ['DOO', 'COO'], [ 'BOO', 'DOO'] ,['DOO', 'BOO'], ['BOO', 'ICN' ], ['COO', 'BOO']]
