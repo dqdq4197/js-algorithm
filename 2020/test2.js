d55f6e9335e1b5c0b81baf620ca5aecf72461d0b
choiheesu <dqdq4197@naver.com>
2021-01-19 05:32:40 +0900

    two pointer algorithm

--- 2020/test2.js
+++ 2020/test2.js
@@ -1,10 +1,45 @@
-arr = [1,2,3,4,5,10,11,12,13,14,15,16];
-let cnt = 0;
-arr.reduce((temp, cur, i) => {
-  if(cur > 10) {
-    return cnt ++;
+function solution(gems) {
+  var answer = [1, gems.length];
+  let gemSet = new Map();
+  gemSet.set(gems[0], 1);
+  let uniqGems = gems.filter((v, i , arr) => arr.indexOf(v) === i).length;
+  
+  let left = 0;
+  let right = 0;
+  
+  while(left < gems.length) {
+    console.log(gemSet)
+      if(uniqGems === gemSet.size) {
+          if(right - left < answer[1] - answer[0]) {
+              answer = [left + 1, right + 1];
+          }
+          let stack = gemSet.get(gems[left])
+          console.log('as', left, right , stack)
+          if(stack > 1) {
+              gemSet.set(gems[left], stack - 1)
+          } else {
+              gemSet.delete(gems[left])
+          }
+          left ++;
+      } else if(right === gems.length - 1) {
+          break;
+      }
+      if(uniqGems > gemSet.size && right !== gems.length - 1) {
+          console.log(left, right)
+          right ++;
+          if(gemSet.has(gems[right])) {
+              gemSet.set(gems[right], gemSet.get(gems[right]) + 1)
+          } else {
+              gemSet.set(gems[right], 1)
+          }
+          console.log('asd', gemSet)
+      }
+      if(left >= right && right !== 0) {
+          break;
+      }
   }
-}) 
-console.log(cnt)
+  
+  return answer;
+}
 
-// 1 100 101 90 
+solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])
