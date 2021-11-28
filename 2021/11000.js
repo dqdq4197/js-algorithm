e9b30a08f85ca6060dba09d6b5241241c711c349
choiheesu <dqdq4197@naver.com>
2021-11-28 17:00:22 +0900

    백준 우선순위 큐(minHeap)문제

--- 2021/11000.js
+++ 2021/11000.js
@@ -6,19 +6,41 @@ const rl = readline.createInterface({
 
 let N;
 let input = [];
+let queue = [0];
 let result = 0;
 
-function enqueue(arr, num) {
-  let isContain = false;
-
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] >= num) {
-      arr.splice(i, 0, num);
-      isContain = true;
-      break;
-    }
+function enqueue(num) {
+  queue.push(num);
+  let size = queue.length - 1;
+  while(size > 1 && queue[Math.floor(size / 2)] > queue[size]) {
+    let temp = queue[Math.floor(size / 2)];
+    queue[Math.floor(size /2)] = queue[size];
+    queue[size] = temp;
+    size = Math.floor(size / 2);
   }
-  if(!isContain) arr.push(num);
+}
+
+function dequeue() {
+  if(queue.length === 1) return undefined;
+  if(queue.length === 2) return queue.pop();
+  let removeItem = queue[1];
+  queue[1] = queue.pop();
+  let p = 1;
+  let c = 2;
+
+  while(c < queue.length) {
+    if(c + 1 < queue.length && queue[c + 1] < queue[c]) {
+      c = c + 1;
+    }
+    if(queue[p] < queue[c]) break;
+    let temp = queue[p];
+    queue[p] = queue[c];
+    queue[c] = temp;
+    p = c;
+    c *= 2;
+  }
+
+  return removeItem;
 }
 
 rl.on('line', function (line) {
@@ -33,18 +55,21 @@ rl.on('line', function (line) {
 .on('close', function () {
   input.sort((a, b) => a[0] - b[0]);
 
-  let queue = [];
-  let idx = 0;
-
   for(let i = 0 ; i < input.length; i++) {
     let [s, t] = input[i];
+    let item = dequeue();
+    if(item === undefined) {
+      result++;
+      enqueue(t);
+      continue;
+    }
 
-    if(queue.length === 0 || queue[idx] > s) {
-      enqueue(queue, t)
+    if(item > s) {
+      enqueue(t);
+      enqueue(item);
       result++;
     } else {
-      enqueue(queue, t);
-      idx++;
+      enqueue(t);
     }
   }
 
