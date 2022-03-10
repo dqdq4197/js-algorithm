9dccede82f8446d61046df7745c1269e4a983900
choiheesu <dqdq4197@naver.com>
2022-03-10 23:20:00 +0900

    백준 크루스칼 문제 답안 약간 수정

--- problems/4386.js
+++ problems/4386.js
@@ -7,51 +7,6 @@ const rl = readline.createInterface({
 let N;
 let input = [];
 
-function minHeap(queue) {
-  this.queue = queue;
-
-  this.getQueue = () => this.queue;
-  this.enqueue = (q) => {
-    this.queue.push(q);
-    let size = this.queue.length - 1;
-
-    while (
-      size > 1 &&
-      this.queue[Math.floor(size / 2)][2] > this.queue[size][2]
-    ) {
-      const temp = this.queue[Math.floor(size / 2)];
-      this.queue[Math.floor(size / 2)] = this.queue[size];
-      this.queue[size] = temp;
-      size = Math.floor(size / 2);
-    }
-  };
-
-  this.dequeue = () => {
-    if (this.queue.length === 2) return this.queue.pop();
-    const removeItem = this.queue[1];
-    this.queue[1] = this.queue.pop();
-    let p = 1;
-    let c = 2;
-
-    while (this.queue.length > c) {
-      if (
-        this.queue.length > c + 1 &&
-        this.queue[c + 1][2] < this.queue[c][2]
-      ) {
-        c = c + 1;
-      }
-      if (this.queue[c][2] > this.queue[p][2]) break;
-      let temp = this.queue[c];
-      this.queue[c] = this.queue[p];
-      this.queue[p] = temp;
-      p = c;
-      c = p * 2;
-    }
-
-    return removeItem;
-  };
-}
-
 function unionFind(N) {
   this.parents = Array.from({ length: N }, (_, i) => i);
 
@@ -85,8 +40,8 @@ rl.on("line", function (line) {
     if (input.length === N) rl.close();
   }
 }).on("close", function () {
-  const minQ = new minHeap([null]);
   const uf = new unionFind(N);
+  let queue = [];
 
   for (let i = 0; i < N; i++) {
     for (let j = i + 1; j < N; j++) {
@@ -96,14 +51,16 @@ rl.on("line", function (line) {
       const dist = +Math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2).toFixed(
         2
       );
-      minQ.enqueue([i, j, dist]);
+      queue.push([i, j, dist]);
     }
   }
 
   let result = 0;
   let cnt = 0;
+  let index = 0;
+  queue.sort((a, b) => a[2] - b[2]);
   while (true) {
-    const [a, b, dist] = minQ.dequeue();
+    const [a, b, dist] = queue[index++];
 
     if (!uf.find(a, b)) {
       result += dist;
