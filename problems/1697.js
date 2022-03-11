25cc84a082ab062f3327165c62f0f3570ea7a11c
choiheesu <dqdq4197@naver.com>
2022-03-11 17:44:34 +0900

    백준 bfs문제  답안 조금 수정

--- problems/1697.js
+++ problems/1697.js
@@ -4,58 +4,15 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function minHeap(q) {
-  this.queue = q;
-
-  this.enqueue = (arr) => {
-    this.queue.push(arr);
-    let size = this.queue.length - 1;
-
-    while (
-      size > 1 &&
-      this.queue[Math.floor(size / 2)][1] > this.queue[size][1]
-    ) {
-      const temp = this.queue[Math.floor(size / 2)];
-      this.queue[Math.floor(size / 2)] = this.queue[size];
-      this.queue[size] = temp;
-      size = Math.floor(size / 2);
-    }
-  };
-
-  this.dequeue = () => {
-    if (this.queue.length === 2) return this.queue[1];
-    const removeItem = this.queue[1];
-    this.queue[1] = this.queue.pop();
-    let p = 1;
-    let c = 2;
-
-    while (this.queue.length > c) {
-      if (
-        this.queue.length > c + 1 &&
-        this.queue[c + 1][1] < this.queue[c][1]
-      ) {
-        c = c + 1;
-      }
-
-      if (this.queue[c][1] > this.queue[p][1]) break;
-      const temp = this.queue[c];
-      this.queue[c] = this.queue[p];
-      this.queue[p] = temp;
-      p = c;
-      c *= 2;
-    }
-    return removeItem;
-  };
-}
-
 rl.on("line", function (line) {
   const [N, K] = line.split(" ").map((n) => +n);
-  const minQ = new minHeap([null]);
   visit = [];
 
-  minQ.enqueue([N, 0]);
-  while (minQ.queue.length > 1) {
-    const [n, time] = minQ.dequeue();
+  let queue = [[N, 0]];
+  let index = 0;
+
+  while (queue.length > index) {
+    const [n, time] = queue[index++];
 
     if (visit[n]) continue;
     visit[n] = true;
@@ -64,9 +21,9 @@ rl.on("line", function (line) {
       break;
     }
 
-    if (n - 1 >= 0) minQ.enqueue([n - 1, time + 1]);
-    if (n !== 0 && n * 2 <= 100000) minQ.enqueue([n * 2, time + 1]);
-    if (n + 1 <= 100000) minQ.enqueue([n + 1, time + 1]);
+    if (n - 1 >= 0) queue.push([n - 1, time + 1]);
+    if (n !== 0 && n * 2 <= 100000) queue.push([n * 2, time + 1]);
+    if (n + 1 <= 100000) queue.push([n + 1, time + 1]);
   }
 
   rl.close();
