611b8fdab706f1bce672d096d4eed52f8e7615ef
choiheesu <dqdq4197@naver.com>
2022-05-27 16:51:32 +0900

    백준 DP + DFS문제

--- problems/17090.js
+++ problems/17090.js
@@ -10,22 +10,19 @@ const dx = [0, 1, 0, -1];
 const dy = [-1, 0, 1, 0]; // U R D L
 const dir = { U: 0, R: 1, D: 2, L: 3 };
 let dp = [];
-let visit = [];
 
 function dfs(y, x) {
-  if (dp[y][x]) return true;
+  if (dp[y][x] !== -1) return dp[y][x];
+  dp[y][x] = 0;
 
   const nDir = dir[input[y][x]];
   const nx = x + dx[nDir];
   const ny = y + dy[nDir];
 
-  if (!visit[y][x]) {
-    visit[y][x] = true;
-    if (nx < 0 || ny < 0 || nx >= M || ny >= N) {
-      dp[y][x] = true;
-    } else {
-      dp[y][x] = dfs(ny, nx);
-    }
+  if (nx < 0 || ny < 0 || nx >= M || ny >= N) {
+    dp[y][x] = 1;
+  } else {
+    dp[y][x] = dfs(ny, nx);
   }
   return dp[y][x];
 }
@@ -33,8 +30,7 @@ function dfs(y, x) {
 rl.on("line", function (line) {
   if (!N) {
     [N, M] = line.split(" ").map(Number);
-    dp = Array.from({ length: N }, () => Array(M).fill(false));
-    visit = Array.from({ length: N }, () => Array(M).fill(false));
+    dp = Array.from({ length: N }, () => Array(M).fill(-1));
   } else {
     input.push(line.split(""));
 
