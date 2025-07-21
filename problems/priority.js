4de7ee22f42f64aa3922613efbf9430cb77021d0
choiheesu <dqdq4197@naver.com>
2025-07-21 15:48:47 +0900

    priority 로직 class로 변경

--- problems/priority.js
+++ problems/priority.js
@@ -1,58 +1,76 @@
-// 왼쪽 자식노드 인덱스 : 부모노드 인덱스 * 2  
-// 오른쪽 자식노드 인덱스 : (부모노드 인덱스 * 2) + 1 
-// 부모노드 인덱스 : 자식노드 / 2 소수점 버림 
+// 왼쪽 자식노드 인덱스 : 부모노드 인덱스 * 2
+// 오른쪽 자식노드 인덱스 : (부모노드 인덱스 * 2) + 1
+// 부모노드 인덱스 : 자식노드 / 2 소수점 버림
 
-let array = [null];
-
-function enqueue(item) {
-  let size = array.length;
-
-  while(array[Math.floor(size / 2)] <= item && size !== 1) {
-    array[size] = array[Math.floor(size / 2)];
-    size = Math.floor(size / 2);
+class PriorityQueue {
+  constructor(compare = (a, b) => a - b) {
+    this.heap = [null];
+    this.compare = compare;
   }
 
-  array[size] = item;
-}
+  enqueue(item) {
+    let size = this.heap.length;
 
-function dequeue(){
-  let remove = array[1];
-  let temp = array.pop();
-  let size = array.length;
-  array[1] = temp; 
-  size--;
+    while (
+      size > 1 &&
+      this.compare(item, this.heap[Math.floor(size / 2)]) < 0
+    ) {
+      this.heap[size] = this.heap[Math.floor(size / 2)];
+      size = Math.floor(size / 2);
+    }
 
-  let parent = 1; 
-  let childe = 2;
-  
-  while(childe <= size){
-      // 두 자식중 큰 노드와 부모노드랑 비교 
-      if(array[childe] < array[childe + 1] && childe < size){
-          childe += 1; 
+    this.heap[size] = item;
+  }
+
+  dequeue() {
+    if (this.heap.length <= 1) {
+      return null;
+    }
+
+    const root = this.heap[1];
+    const last = this.heap.pop();
+
+    if (this.heap.length === 1) {
+      return root;
+    }
+
+    let parent = 1;
+    let child = 2;
+
+    while (child < this.heap.length) {
+      if (
+        child + 1 < this.heap.length &&
+        this.compare(this.heap[child + 1], this.heap[child]) < 0
+      ) {
+        child++;
       }
 
-      if(temp >= array[childe]) break; // 만약 자식 노드와 비교해서 크다면 해당 반복문 멈춤 
-      array[parent] = array[childe]; 
-      parent = childe;
-      childe *=2;
+      if (this.compare(last, this.heap[child]) <= 0) break;
+
+      this.heap[parent] = this.heap[child];
+      parent = child;
+      child *= 2;
+    }
+
+    this.heap[parent] = last;
+    return root;
   }
-  array[parent] = temp;
-  // console.log(array);
-  return remove;
+
+  size() {
+    return this.heap.length - 1;
+  }
 }
 
-enqueue(19);
-enqueue(12);
-enqueue(13);
-enqueue(15);
-enqueue(9);
-enqueue(20);
-console.log('as', array);
-console.log(dequeue());
-console.log(dequeue());
-console.log(dequeue());
-console.log(dequeue());
-console.log(dequeue());
+const heap = new PriorityQueue();
 
-console.log(array);
-// size => 3, item => 2
+heap.enqueue(19);
+heap.enqueue(12);
+heap.enqueue(13);
+heap.enqueue(15);
+heap.enqueue(9);
+heap.enqueue(20);
+console.log(heap.dequeue());
+console.log(heap.dequeue());
+console.log(heap.dequeue());
+console.log(heap.dequeue());
+console.log(heap.dequeue());
