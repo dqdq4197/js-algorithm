f276c25a6345a3133de3814a990bcafb8d5162d5
choiheesu <dqdq4197@naver.com>
2025-08-02 20:06:06 +0900

    mockup 수정

--- problems/mockup.js
+++ problems/mockup.js
@@ -1,32 +1,18 @@
-/**
- * 백준 - DP
- * https://www.acmicpc.net/problem/2747
- */
-
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-function solution(n) {
-  const numbers = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    const p1 = numbers[i - 1];
-    const p2 = numbers[i - 2];
-
-    numbers.push(p1 + p2);
-  }
-
-  return numbers[n];
-}
+let N, M;
+let input = [];
 
 rl.on("line", function (line) {
-  const n = +line;
-
-  console.log(solution(n));
-  rl.close();
+  if (!N) {
+    [N, M] = line.split(" ").map(Number);
+  } else {
+    input.push(line);
+  }
 }).on("close", function () {
   process.exit();
 });
