6dc8b0fd822d0b524e82d5f0f811027578a9c2d2
choiheesu <dqdq4197@naver.com>
2025-08-04 15:41:33 +0900

    백준: 6087번 - 골드3 (bfs)

--- problems/6087.js
+++ problems/6087.js
@@ -1,3 +1,8 @@
+/**
+ * 백준 - bfs
+ * https://www.acmicpc.net/problem/6087
+ */
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
@@ -5,55 +10,84 @@ const rl = readline.createInterface({
 });
 
 let W, H;
-let input = [];
-const dx = [1, 0, -1, 0];
-const dy = [0, 1, 0, -1];
-const INF = Infinity;
-
-function bfs(start) {
-  const queue = [[...start, 0, null]];
-  let index = 0;
-
-  while (queue.length > index) {
-    const [y, x, cnt, dir] = queue[index++];
-
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-      let nCnt = cnt;
-
-      if (nx < 0 || ny < 0 || nx >= W || ny >= H || input[ny][nx] === "*")
-        continue;
-      if (dir !== null && dir !== i) nCnt++;
-      if (visit[ny][nx] < nCnt) continue;
-      visit[ny][nx] = nCnt;
-      queue.push([ny, nx, nCnt, i]);
-    }
-  }
-}
+let start, end;
+let map = [];
+//          상, 하, 좌, 우
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
 
 rl.on("line", function (line) {
   if (!W) {
     [W, H] = line.split(" ").map(Number);
   } else {
-    input.push(line.split(""));
-    if (input.length === H) rl.close();
+    const row = line.split("");
+    map.push(row);
+
+    for (let i = 0; i < W; i++) {
+      const cell = row[i];
+
+      if (cell === "C") {
+        const pos = [i, map.length - 1];
+        if (!start) {
+          start = pos;
+        } else {
+          end = pos;
+        }
+      }
+    }
+
+    if (map.length === H) {
+      rl.close();
+    }
   }
 }).on("close", function () {
-  visit = Array.from({ length: H }, () => Array(W).fill(INF));
-  let start, end;
+  const [sx, sy] = start;
+  const [ex, ey] = end;
+  const visited = Array.from({ length: H }, () =>
+    Array.from({ length: W }, () => Array(4).fill(Infinity))
+  );
 
-  for (let i = 0; i < H; i++) {
-    for (let j = 0; j < W; j++) {
-      if (input[i][j] === "C") {
-        if (!start) {
-          start = [i, j];
-          visit[i][j] = 0;
-        } else end = [i, j];
+  visited[sy][sx] = Array(4).fill(0);
+  const queue = Array.from({ length: 4 }, (_, i) => [sx, sy, i]);
+
+  let index = 0;
+  while (queue.length > index) {
+    const [x, y, dir] = queue[index++];
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx < 0 || ny < 0 || nx >= W || ny >= H) {
+        continue;
+      }
+
+      if (map[ny][nx] === "*") {
+        continue;
+      }
+
+      const cost = visited[y][x][dir];
+      const nCost = dir === i ? cost : cost + 1;
+
+      if (visited[ny][nx][i] <= nCost) {
+        continue;
+      }
+
+      visited[ny][nx][i] = nCost;
+      queue.push([nx, ny, i]);
+    }
+  }
+
+  let result;
+  for (let y = 0; y < H; y++) {
+    for (let x = 0; x < W; x++) {
+      if (y === ey && x === ex) {
+        result = Math.min(...visited[y][x]);
       }
     }
   }
-  bfs(start);
-  console.log(visit[end[0]][end[1]]);
+
+  console.log(result);
+
   process.exit();
 });
