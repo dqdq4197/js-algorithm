7bb688f65d24b3b50c955a2db28f4bddb1490528
choiheesu <dqdq4197@naver.com>
2025-08-11 14:28:21 +0900

    백준: 1647번 - 골드4 (최소 스패닝 트리 / 크루스칼)

--- problems/1647.js
+++ problems/1647.js
@@ -1,3 +1,8 @@
+/**
+ * 백준 - 최소 스패닝 트리 (크루스칼)
+ * https://www.acmicpc.net/problem/1647
+ */
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
@@ -5,57 +10,64 @@ const rl = readline.createInterface({
 });
 
 let N, M;
-let input = [];
-let parents = [];
-function getParent(num) {
-  if (parents[num] === num) return num;
-  return (parents[num] = getParent(parents[num]));
-}
+let edges = [];
 
-function unionParent(a, b) {
-  const aParent = getParent(a);
-  const bParent = getParent(b);
-  if (aParent > bParent) parents[aParent] = bParent;
-  else parents[bParent] = aParent;
-}
-
-function findParent(a, b) {
-  const aParent = getParent(a);
-  const bParent = getParent(b);
-
-  if (aParent === bParent) return true;
-  else return false;
-}
-
-function kruskal() {
-  let sum = 0;
-  let cnt = 0;
-
-  for (let i = 0; i < M; i++) {
-    const [a, b, cost] = input[i];
-    if (!findParent(a, b)) {
-      sum += cost;
-      unionParent(a, b);
-      cnt += 1;
-    }
-    if (cnt === N - 2) break;
+class UnionFind {
+  constructor(n) {
+    this.parents = Array.from({ length: n + 1 }, (_, i) => i);
   }
 
-  return sum;
+  find(node) {
+    if (this.parents[node] === node) return node;
+    else return (this.parents[node] = this.find(this.parents[node]));
+  }
+
+  union(a, b) {
+    const aParent = this.find(a);
+    const bParent = this.find(b);
+
+    if (aParent === bParent) {
+      return false;
+    }
+
+    if (aParent > bParent) {
+      this.parents[aParent] = bParent;
+    } else {
+      this.parents[bParent] = aParent;
+    }
+    return true;
+  }
 }
 
 rl.on("line", function (line) {
   if (!N) {
-    [N, M] = line.split(" ").map((n) => +n);
-    parents = Array.from({ length: N + 1 }, (_, i) => i);
+    [N, M] = line.split(" ").map(Number);
   } else {
-    input.push(line.split(" ").map((n) => +n));
+    edges.push(line.split(" ").map(Number));
 
-    if (input.length === M) {
-      input.sort((a, b) => a[2] - b[2]);
-      const result = kruskal();
-      console.log(result);
+    if (edges.length === M) {
       rl.close();
     }
   }
+}).on("close", function () {
+  edges.sort((a, b) => a[2] - b[2]);
+
+  let totalCost = 0;
+  let connectedEdgesCount = 0;
+  const unionFind = new UnionFind(N);
+
+  for (let i = 0; i < M; i++) {
+    const [a, b, cost] = edges[i];
+
+    if (connectedEdgesCount === N - 2) {
+      break;
+    }
+
+    if (unionFind.union(a, b)) {
+      totalCost += cost;
+      connectedEdgesCount += 1;
+    }
+  }
+
+  console.log(totalCost);
 });
