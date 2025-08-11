676fbc2bdafe35122796cbcc53168b9823f59506
choiheesu <dqdq4197@naver.com>
2025-08-11 13:41:57 +0900

    union-find 알고리즘 리팩터링

--- algorithms/union-find.js
+++ algorithms/union-find.js
@@ -3,6 +3,21 @@
  * 초기 값으로 자기 자신을 부모로 설정
  * 두 개의 노드중 더 작은 노드가 부모로 설정하는것이 원칙.
  */
+
+const N = 8;
+
+/**
+ * edges:
+ *
+ *     1
+ *    / \
+ *   2---3
+ *   |  /|\
+ *   | / | \
+ *   4---5---6
+ *
+ *   7---8
+ */
 const edges = [
   [1, 2],
   [1, 3],
@@ -15,42 +30,65 @@ const edges = [
   [5, 6],
   [8, 7],
 ];
-const nodeCnt = 8;
-let parents = Array.from({ length: nodeCnt + 1 }, (_, i) => i);
 
-function getParent(num) {
-  if (parents[num] === num) return num;
-  return (parents[num] = getParent(parents[num]));
-}
+class UnionFind {
+  constructor(n) {
+    this.parents = Array.from({ length: n + 1 }, (_, i) => i);
+  }
 
-function unionParent(a, b) {
-  const aParent = getParent(a);
-  const bParent = getParent(b);
-  if (aParent > bParent) parents[aParent] = bParent;
-  else parents[bParent] = aParent;
-}
+  find(node) {
+    if (this.parents[node] === node) return node;
+    return (this.parents[node] = this.find(this.parents[node]));
+  }
 
-function findParent(a, b) {
-  const aParent = getParent(a);
-  const bParent = getParent(b);
+  union(a, b) {
+    const aParent = this.find(a);
+    const bParent = this.find(b);
 
-  if (aParent === bParent) return true;
-  else return false;
-}
+    if (aParent === bParent) {
+      return false;
+    }
 
-for (let i = 0; i < edges.length; i++) {
-  const [a, b] = edges[i];
-  if (findParent(a, b)) console.log(a, b);
-  if (!findParent(a, b)) {
-    unionParent(a, b);
+    if (aParent > bParent) {
+      this.parents[aParent] = bParent;
+    } else {
+      this.parents[bParent] = aParent;
+    }
+
+    return true;
+  }
+
+  connected(a, b) {
+    return this.find(a) === this.find(b);
   }
 }
 
-console.log(parents);
-console.log(`1과 4가 연결되어있나요? ${findParent(1, 4) ? "네" : "아니요"}`);
-console.log(`1과 7이 연결되어있나요? ${findParent(1, 7) ? "네" : "아니요"}`);
-console.log(`1과 8이 연결되어있나요? ${findParent(1, 8) ? "네" : "아니요"}`);
-console.log(`7과 8이 연결되어있나요? ${findParent(7, 8) ? "네" : "아니요"}`);
-unionParent(1, 8);
-console.log(`1과 7이 연결되어있나요? ${findParent(1, 7) ? "네" : "아니요"}`);
-console.log(`1과 8이 연결되어있나요? ${findParent(1, 8) ? "네" : "아니요"}`);
+const unionFind = new UnionFind(N);
+
+for (let i = 0; i < edges.length; i++) {
+  const [a, b] = edges[i];
+
+  unionFind.union(a, b);
+}
+
+console.log(unionFind.parents);
+console.log(
+  `1과 4가 연결되어있나요? ${unionFind.connected(1, 4) ? "네" : "아니요"}`
+);
+console.log(
+  `1과 7이 연결되어있나요? ${unionFind.connected(1, 7) ? "네" : "아니요"}`
+);
+console.log(
+  `1과 8이 연결되어있나요? ${unionFind.connected(1, 8) ? "네" : "아니요"}`
+);
+console.log(
+  `7과 8이 연결되어있나요? ${unionFind.connected(7, 8) ? "네" : "아니요"}`
+);
+unionFind.union(1, 8);
+console.log("1과 8 연결 진행.");
+console.log(
+  `1과 7이 연결되어있나요? ${unionFind.connected(1, 7) ? "네" : "아니요"}`
+);
+console.log(
+  `1과 8이 연결되어있나요? ${unionFind.connected(1, 8) ? "네" : "아니요"}`
+);
